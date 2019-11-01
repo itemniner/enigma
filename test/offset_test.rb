0a75285583991a5faa76a020c6a66b8ee28585da
Raymond Nguyen <rayknguyen@gmail.com>
2019-11-01 11:34:50 -0600

    Adds test file for offset test

---
+++ test/offset_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/enigma'
+require './lib/offset'
+require 'date'
+
+class Offset < Minitest::Test
+  #takes in the date
+  #multiple that by it self
+  #grabs the last 4 numbers of that
+end
