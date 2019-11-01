bd6f662075401156fee91db9e73883e96b24adc3
Raymond Nguyen <rayknguyen@gmail.com>
2019-11-01 11:34:24 -0600

    Adds test file for enigma

---
+++ test/enigma_test.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/engima'
+require './lib/offset'
+require './lib/key'
+
+class EnigmaTest < Minitest::Test
+  # def test_it_exist
+  #   assert_instance_of Enigma,
+  # end
+
+  #takes in key,date,message
+end
