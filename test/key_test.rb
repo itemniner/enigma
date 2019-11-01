7968fc483e5f8c9242070187ab2b547807f5fb82
Raymond Nguyen <rayknguyen@gmail.com>
2019-11-01 11:35:04 -0600

    Adds file for key test

---
+++ test/key_test.rb
@@ -0,0 +1,9 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/enigma'
+require './lib/key'
+
+class Key < Minitest::Test
+#generate random key that is used untill invoke the method to change
+  #key is 5 digits that uses each cons to retrieve => final key" example 12345 12 23 34 45
+end
