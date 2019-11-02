e0f2a0b47b384befa41294e222380ad9d45f74d3
Raymond Nguyen <rayknguyen@gmail.com>
2019-11-02 09:25:12 -0600

    Adds attributes for enigma

---
+++ lib/enigma.rb
@@ -0,0 +1,8 @@
+class Enigma
+  attr_reader :message, :key, :date
+  def initialize(message, key, date)
+    # @encryption = traits[encryption]
+    # @key = traits[key]
+    # @date = traits[date]
+  end
+end
