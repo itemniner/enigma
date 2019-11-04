99b6ae9e6d64ada855b1912b50eb3aa0487c9c18
Raymond Nguyen <rayknguyen@gmail.com>
2019-11-03 20:33:24 -0700

    Adds test to key test file

--- test/key_test.rb
+++ test/key_test.rb
@@ -5,7 +5,16 @@ require './lib/key'
 #generate random key that is used untill invoke the method to change
   #key is 5 digits that uses each cons to retrieve => final key" example 12345 12 23 34 45
 
-class Key < Minitest::Test
+class KeyTest < Minitest::Test
+  def setup
+    @key = "02715"
+  end
+  def test_it_can_get_split_key
+    assert_equal [02, 27, 71, 15], @key.seprarate
+  end
+  # def test_it_can_get_five_digit
+  #   assert_equal 5, @key.random_five_digit
+  # end
   # def test_it_can_grab_random_five_number
   #   key.stubs(:get_five_numbers).returns("59376")
   #   assert_equal "59376", @key.get_five_numbers
