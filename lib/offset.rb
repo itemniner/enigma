bdde848af789d7c0454cac7ddfcca98e07951570
Raymond Nguyen <rayknguyen@gmail.com>
2019-11-04 20:53:21 -0700

    Adds method to offset file

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,2 +1,11 @@
+require 'date'
 class Offset
+  def todays_date
+    date = Time.now.strftime("%d/%m/%y")
+    date.tr('/', '').to_i
+  end
+
+  def square_date(date)
+    todays_date * todays_date.round(2)
+  end
 end
