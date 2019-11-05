f92e94099b2765593d305624f242c1a80a052624
Raymond Nguyen <rayknguyen@gmail.com>
2019-11-05 13:39:06 -0700

    Adds method for shift file and test

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -1,13 +1,15 @@
 require_relative 'test_helper'
 
 class ShiftTest < Minitest::Test
-  # def setup
-  #   key = {"A" => 02, "B"=> 27, "C"=> 71, "D"=> 15}
-  #   offset = {"A" => 2, "B"=> 1, "C"=> 6, "D"=> 1}
-  # end
-  #
-  # def test_it_can_add_key_and_date
-  #   expected = {"A" => 04, "B"=> 28, "C"=> 77, "D"=> 16}
-  #   assert_equal expected, @key.pair_keys(key)
-  # end
+
+  def setup
+    @shift = Shift.new
+  end
+
+  def test_it_can_add_key_and_date
+    key = {"A" => 02, "B"=> 27, "C"=> 71, "D"=> 15}
+    offset = {"A" => 2, "B"=> 1, "C"=> 6, "D"=> 1}
+    expected = {"A" => 04, "B"=> 28, "C"=> 77, "D"=> 16}
+    assert_equal expected, @shift.shift_value(key, offset)
+  end
 end
