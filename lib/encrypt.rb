c27bb1b476adfd86bb8463d7c9ebac2fe8523a3d
Raymond Nguyen <rayknguyen@gmail.com>
2019-11-06 06:22:49 -0700

    Adds interface for enignma

---
+++ lib/encrypt.rb
@@ -0,0 +1,17 @@
+
+require './lib/enigma'
+require './lib/shift'
+require './lib/key'
+require './lib/offset'
+
+(message, where)= ARGV
+
+
+messages = File.open(message, "r")
+
+encrypted = File.open(where, "w")
+encrypted_message = Enigma.new.encrypt(messages.read.chomp)
+encrypted.write(encrypted_message[:encryption])
+encrypted.close
+
+p "Created #{where} with the key #{encrypted_message[:key]} and date #{encrypted_message[:date]}"
