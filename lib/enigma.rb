9cd1ff0173945ac1ea3b92701f1edd85495cb50c
Raymond Nguyen <rayknguyen@gmail.com>
2019-11-06 06:49:04 -0700

    Add decrypt interface

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,11 +3,11 @@ class Enigma
   def encrypt(message, key = Key.new.generate_random_key, date = Offset.new.todays_date)
     hashes = get_hashes(key,date)
       {encryption: Shift.new.shift_characters(message, hashes[0], hashes[1]),
-       key: key.to_s,
+       key: key,
        date: date}
   end
 
-  def decrypt(message, key, date = Offset.new.todays_date)
+  def decrypt(message, key = Key.new.generate_random_key, date = Offset.new.todays_date)
     hashes = get_hashes(key,date)
       {decryption: Shift.new.negative_shift_characters(message, hashes[0], hashes[1]),
        key: key,
