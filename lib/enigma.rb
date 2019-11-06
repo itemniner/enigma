7d94a6a027a2184e0e935059e64a478384e98c01
Raymond Nguyen <rayknguyen@gmail.com>
2019-11-05 21:53:30 -0700

    Refactor enigma file

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,20 +1,20 @@
 class Enigma
 
-  def encrypt(message, key, date)
+  def encrypt(message, key = Key.new.generate_random_key, date = Offset.new.todays_date)
     hashes = get_hashes(key,date)
       {encryption: Shift.new.shift_characters(message, hashes[0], hashes[1]),
+       key: key.to_s,
+       date: date}
+  end
+
+  def decrypt(message, key, date = Offset.new.todays_date)
+    hashes = get_hashes(key,date)
+      {decryption: Shift.new.negative_shift_characters(message, hashes[0], hashes[1]),
        key: key,
        date: date }
   end
 
   def get_hashes(key, date)
     [Key.new.pair_keys(key), Offset.new.pair_keys(date.to_i)]
-  end
-
-  def decrypt(message, key, date)
-    hashes = get_hashes(key,date)
-      {decryption: Shift.new.negative_shift_characters(message, hashes[0], hashes[1]),
-       key: key,
-       date: date }
   end
 end
