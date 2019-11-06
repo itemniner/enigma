d5486e680c6e5b11adee3fb0057a75b097a6f218
Raymond Nguyen <rayknguyen@gmail.com>
2019-11-05 18:45:08 -0700

    Adds method for encryption

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,3 +1,13 @@
 class Enigma
-  
+
+  def encrypt(message, key, date)
+    hashes = get_hashes(key,date)
+    {encryption: Shift.new.shift_characters(message, hashes[0], hashes[1]),
+       key: key,
+      date: date }
+  end
+
+  def get_hashes(key, date)
+    [Key.new.pair_keys(key), Offset.new.pair_keys(date.to_i)]
+  end
 end
